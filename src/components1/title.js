67c593d9ac6b3371dd6e36aed8dbc0bccf19c8ea
JuanRoque240600 <roquebautista65@gmail.com>
2022-05-21 09:07:46 -0500

    actualizacion

--- src/components1/title.js
+++ src/components1/title.js
@@ -3,7 +3,7 @@ import React from "react";
 function Title() {
   return (
     <h1 className="title">
-      NUMERO DE EMPLEADO{" "}
+      PASE SU CODIGO QR POR EL SCANNER O IGRESE SU NUMERO DE EMEPLEADO{" "}
       
     </h1>
   );
